7ab993a9759403cecd90af2b1cfff49e97a850b9
SurpassJy <960699586@qq.com>
2025-03-26 17:28:24 +0800

    finish the ten sort method

--- Enhanced/DataStructureAndAlgorithm/Sort/Sort.cpp
+++ Enhanced/DataStructureAndAlgorithm/Sort/Sort.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 #include<list>
+#include<algorithm>
 using namespace std;
 
 void insertionSort(vector<int> &vec) {
@@ -213,16 +214,141 @@ void heapSort(vector<int> &nums) {
         siftDown(nums,i,0);
     }
 }
+//bucket sort
+void bucketSort(vector<double> vec) {
+    //the num inside vec is from 0 to 1
+    //creater n/2 buckets
+    int k = vec.size() / 2;
+    vector<vector<double>> buckets(k);
+    //using num*k to map the num to buckets
+    for (double num : vec) {
+        buckets[num * k].push_back(num);
+    }
+    //sort every small bucket
+    for (int i = 0;i < k;i++) {
+        sort(buckets[i].begin(),buckets[i].end());
+    }
+    for (int i = 0;i < buckets.size();i++) {
+        for (int j = 0;j < buckets[i].size();j++) {
+            cout << buckets[i][j] << " ";
+        }
+    }
+}
+void countingSort(vector<int>& nums)
+{
+    //1.get the max value
+    int m = 0;
+    for (int num : nums) {
+        m = max(num, m);
+    }
+    //2.create an array whose size is m
+    vector<int> counter(m + 1, 0);
+    for (int num : nums)
+    {
+        counter[num]++;
+    }
+    //traverse the counter and put the index with its counts to the original nums
+    nums.clear();
+    for (int i=0;i<m+1;i++) 
+    {
+        if (counter[i] != 0)
+            for(int j=0;j<counter[i];j++)
+            nums.push_back(i);
+    }
+}
 
+void fullCountingSort(vector<int>&nums) 
+{
+    //get the max element
+    int m = 0;
+    for (int num:nums) 
+    {
+        m=max(num,m);
+    }
+    //create a vector whose size is m+1 and count num
+    vector<int> counter(m+1,0);
+    //count
+    for (int num:nums) 
+    {
+        counter[num]++;
+    }
+    //change the count of elements to its prefix
+    //this step change the counts of element to its index+1 
+    for (int i=0;i<counter.size()-1;i++) 
+    {
+        counter[i + 1] += counter[i];
+    }
+    //traverse the nums from the last element, insert it to a temp array
+    vector<int> temp(nums.size());
+    for (int i=nums.size()-1;i>=0;i--) 
+    {
+        temp[counter[nums[i]] - 1] = nums[i];
+        counter[nums[i]]--;
+    }
+    //assign the temp to original vector
+    nums = temp;
+}
 
+int getDigit(int num,int exp)//exp=10^k-1
+{
+    return num /exp%10;
+}
+
+void countingSortRadix(vector<int> &nums,int exp) 
+{
+    vector<int> counter(10,0);
+    //get the digit and count the element
+    for (int i=0;i<nums.size();i++) 
+    {
+        int digit = getDigit(nums[i],exp);
+        counter[digit]++;
+    }
+    //get the prefix, turning the counts to index+1
+    for (int i=0;i<9;i++) 
+    {
+        counter[i + 1] += counter[i];
+    }
+    //traverse the original vector reversely,and put element to a new vector
+    vector<int> temp(nums.size(),0);
+    for (int i=nums.size()-1;i>=0;i--) 
+    {
+        //we should get the digit to map the index here
+        int digit = getDigit(nums[i],exp);
+        temp[counter[digit]-1] = nums[i];
+        counter[digit]--;
+    }
+    //assign the temp to original nums
+    for(int i=0;i<nums.size();i++)
+    {
+        nums[i] = temp[i];
+    }
+}
+void radixSort(vector<int> &nums) //适用于整数，且位数一致并较大的时候，如学号
+{
+    //get the max element to get the number of digit
+    int m = 0;
+    for (int num:nums) 
+    {
+        m = max(m, num);
+    }
+    for (int exp=1;exp<=m;exp*=10) 
+    {
+        countingSortRadix(nums,exp);
+    }
+}
 int main() {
     vector<int> vec;
     for (int i = 6; i >= 0; i--) {
         vec.push_back(i);
     }
-    heapSort(vec);
-    for (int i: vec) {
-        cout << i << endl;
+    //vector<double> doubleVec = {0.1,0.3,0.2,0.7,0.5,0.4,0.3};
+    //bucketSort(doubleVec);
+    //fullCountingSort(vec);
+    vector<int> vec2 = { 780245,581746,581735,666666,999999 };
+    radixSort(vec2);
+    for (int i=0;i<vec2.size();i++) 
+    {
+        cout << vec2[i] << " ";
     }
     return 0;
 }
